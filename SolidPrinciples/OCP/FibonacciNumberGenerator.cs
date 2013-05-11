c0f7fcd62ec9ac47f757730b8b18fe58c966fd25
Claus Polanka <claus.polanka@chello.at>
2013-05-11 18:28:56 +0200

    Added OCP example.

---
+++ SolidPrinciples/OCP/FibonacciNumberGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SolidPrinciples.OCP
+{
+    public sealed class FibonacciNumberGenerator
+    {
+        public int GetNumberAt(int positionInSequence)
+        {
+            if (positionInSequence < 2)
+                return positionInSequence;
+
+            return GetNumberAt(positionInSequence - 1) + GetNumberAt(positionInSequence - 2);
+        }
+
+        public int[] GetSequenceOfLength(int length)
+        {
+            if (length < 8 || length > 50)
+                throw new ArgumentException();
+
+            var sequence = new int[length];
+            
+            for (int i = 0; i < length; i++)
+                sequence[i] = GetNumberAt(i);
+
+            return sequence;
+        }
+    }
+}
