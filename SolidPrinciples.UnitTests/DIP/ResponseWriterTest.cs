3f66c2546c32dbec232825e7ca32553862ade51b
Claus Polanka <claus.polanka@chello.at>
2013-05-14 12:33:57 +0200

    Added tests for ResponseWriter

---
+++ SolidPrinciples.UnitTests/DIP/ResponseWriterTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using SolidPrinciples.DIP;
+
+namespace SolidPrinciples.UnitTests.DIP
+{
+    [TestFixture]
+    public class ResponseWriterTest
+    {
+        [Test]
+        public void CreateHtmlResponseForCustomer()
+        {
+            var customer = new Customer { Name = "Name1" };
+            var responseKind = ResponseKind.HTML;
+            string response = new ResponseWriter().CreateResponse(customer, responseKind);
+            Assert.That(response, Is.StringContaining("html"), "response format");
+            Assert.That(response, Is.StringContaining("Name1"), "response");
+        }
+
+        [Test]
+        public void CreateXmlResponseForCustomer()
+        {
+            var customer = new Customer { Name = "Name1" };
+            string response = new ResponseWriter().CreateResponse(customer, null);
+            Assert.That(response, Is.StringContaining("<customer>"), "response format");
+            Assert.That(response, Is.StringContaining("Name1"), "response");
+        }
+    }
+}
--- SolidPrinciples/DIP/ResponseKind.cs
+++ SolidPrinciples/DIP/ResponseKind.cs
@@ -2,6 +2,48 @@ namespace SolidPrinciples.DIP
 {
     public class ResponseKind
     {
-        public const ResponseKind HTML = null;
+        public static ResponseKind HTML = new ResponseKind("html");
+        
+        private readonly string kind;
+
+        private ResponseKind(string kind)
+        {
+            this.kind = kind;
+        }
+
+        public bool Equals(ResponseKind other)
+        {
+            if (ReferenceEquals(null, other))
+                return false;
+            if (ReferenceEquals(this, other))
+                return true;
+            return Equals(other.kind, kind);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            if (obj.GetType() != typeof (ResponseKind))
+                return false;
+            return Equals((ResponseKind) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (kind != null ? kind.GetHashCode() : 0);
+        }
+
+        public static bool operator ==(ResponseKind left, ResponseKind right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(ResponseKind left, ResponseKind right)
+        {
+            return !Equals(left, right);
+        }
     }
 }
